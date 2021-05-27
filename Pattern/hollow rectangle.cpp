4fafbd316cfc6215c937bc023f425bf5829fb758
dexcoder07 <55050907+dexcoder07@users.noreply.github.com>
2021-05-27 17:27:58 +0530

    Add files via upload

---
+++ Pattern/hollow rectangle.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int row, column;
+	cin>>row>>column;
+
+	for(int i = 0 ;  i < row ; i++)
+	{
+		for(int j = 0 ; j < column ; j++)
+		{
+			if((i==0 || i==row-1) || (j==0 || j==column-1))
+			{
+				cout<<"*";
+			}
+			else
+			{
+				cout<<" ";
+			}
+		}
+		cout<<endl;
+	}
+	return 0;
+}
